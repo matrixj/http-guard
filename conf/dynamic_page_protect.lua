724f1dc1af4b8e7332727917b6e0d4fad3e79a09
maohaizhu <admin@centos.bz>
2013-05-19 23:10:37 +0800

    完成动态页的js防cc及防sql注入等

--- conf/dynamic_page_protect.lua
+++ conf/dynamic_page_protect.lua
@@ -1,1 +1,119 @@
-ngx.exec("@php")
+local ip = ngx.var.binary_remote_addr;
+local cookie = ngx.var.cookie_httpguard;
+
+if not cookie then
+	cookie = 100000000;
+end
+
+--请求限速
+if cookie_enable==1 then
+	--计算ip与cookie的md5
+	local durl_flag = ngx.md5(table.concat({ip,cookie}));
+	local baduser,_=bad_user:get(durl_flag);
+	--判断此用户是否在黑名单
+	if baduser then
+		ngx.exit(444);
+	else
+		local durl = d_url:get(durl_flag);
+		--判断a_url字典是否存在
+		if durl then
+			--判断此用户总访问数是否超过限制
+			if durl > d_url_max then
+				--加入黑名单
+				bad_user:set(durl_flag,0,ban_time);
+				--断开连接
+				ngx.exit(444);
+			else
+				--该用户访问此url次数加1
+				d_url:incr(durl_flag,1);
+			end	
+		else
+			--添加记录进a_url词典
+			d_url:set(durl_flag,1,10);
+		end
+	end
+else
+	local durl_flag = ip;
+	local baduser,_=bad_user:get(durl_flag);
+	--判断此用户是否在黑名单
+	if baduser then
+		ngx.exit(444);
+	else
+		local durl = d_url:get(durl_flag);
+		--判断a_url字典是否存在
+		if durl then
+			--判断此用户总访问数是否超过限制
+			if durl > d_url_max then
+				--加入黑名单
+				bad_user:set(durl_flag,0,ban_time);
+				--断开连接
+				ngx.exit(444);
+			else
+				--该用户访问此url次数加1
+				d_url:incr(durl_flag,1);
+			end	
+		else
+			--添加记录进a_url词典
+			d_url:set(durl_flag,1,10);
+		end
+	end
+end	
+
+--请求过滤
+if (ngx.req.get_method()=="GET") then
+	local uri = ngx.var.request_uri;
+	--开启js防cc攻击
+	if jscc==1 then
+		local jsjump = ngx.shared.jsjump
+		local jspara,flags=jsjump:get(ip)
+		local args = ngx.req.get_uri_args()
+		if jspara then
+			if flags then
+				ngx.exec("@php")
+			else
+				local p_jskey=''
+				if args["jskey"] and type(args["jskey"])=='table' then
+						p_jskey=args["jskey"][table.getn(args["jskey"])]
+				else
+						p_jskey=args["jskey"]
+				end
+				if p_jskey and p_jskey==tostring(jspara) then
+					jsjump:set(ip,jspara,white_time,1)
+					ngx.exec("@php")
+				else
+					local url=''
+					if ngx.var.args then
+						url=table.concat({ngx.var.scheme,"://",ngx.var.host,uri,"&jskey=",jspara});
+					else
+						url=table.concat({ngx.var.scheme,"://",ngx.var.host,uri,"?jskey=",jspara});
+					end
+					local jscode=table.concat({"<script>window.location.href='",url,"';</script>"});
+					ngx.say(jscode)
+				end
+			end
+		else
+			math.randomseed( os.time() );
+			local random=math.random(100000,999999)
+			jsjump:set(ip,random,60)
+			local url=''
+			if ngx.var.args then
+				url=table.concat({ngx.var.scheme,"://",ngx.var.host,uri,"&jskey=",random});
+			else
+				url=table.concat({ngx.var.scheme,"://",ngx.var.host,uri,"?jskey=",random});
+			end
+			local jscode=table.concat({"<script>window.location.href='",url,"';</script>"});
+			ngx.say(jscode)
+		end
+	elseif sql_filter==1 then
+		url=ngx.unescape_uri(uri);
+		local m,err = ngx.re.match(url,sql_rule,"i")
+		if m then
+			ngx.exit(444);
+		end
+		ngx.exec("@php");
+	end
+elseif (ngx.req.get_method()=="POST") then
+	ngx.exec("@php");
+else
+	ngx.exec("@php");
+end
--- conf/init.lua
+++ conf/init.lua
@@ -1,11 +1,10 @@
---初始化三个词典
-s_url = ngx.shared.s_url;
-a_url = ngx.shared.a_url;
-bad_user = ngx.shared.bad_user;
+--------------全局设置-----------------
 
-
---黑名单时间,单位:秒.
-ban_time = 600;
+s_url = ngx.shared.s_url; --用于记录单用户请求同一url的次数
+a_url = ngx.shared.a_url; --用于记录单用户所有请求次数
+d_url = ngx.shared.d_url; --用于记录动态页面的请求次数
+bad_user = ngx.shared.bad_user; --黑名单词典
+ban_time = 600; --黑名单时间,单位:秒.
 
 --cookie标记开关,用来解决多用户共享ip上网误判的问题.
 --攻击者也可能利用cookie跳过限制
@@ -14,31 +13,15 @@ cookie_enable = 0;
 
 -----------静态资源变量设置--------------
 
---单个url 10秒内允许最大访问次数
-s_url_max = 10;
-
---10秒内允许的最大总访问次数
-a_url_max = 60;
+s_url_max = 10; --单个url 10秒内允许最大访问次数
+a_url_max = 60; --10秒内允许的最大总访问次数
 
 ------------动态网页变量设置---------------
 
---10秒内允许的最大总访问次数
-d_url_max = 10;
-
---js防cc开关,0为关闭,1为开启.
-jscc = 0;
-
---搜索引擎js防cc白名单
-spider_list="baiduspider|googlebot";
-
---针对搜索引擎不启用js防cc开关,0为关闭,1为开启.
-spider_jscc = 0;
-
---sql防注入开关,0为关闭,1为开启.
-sql_filter = 1;
-
---防止上传特定文件,如php文件,0为关闭,1为开启.
-upload_filter = 1;
-
---禁止上传的文件后缀.
-file_type = "php|jsp";
+d_url_max = 10; --10秒内允许的最大总访问次数
+jscc = 0;       --js防cc开关,0为关闭,1为开启. 
+white_time = 600; -- js跳转验证后白名单的时间.
+--ignore_spider = "baiduspider|googlebot"; --搜索引擎js防cc白名单,注释则不启用
+sql_filter = 1; --sql防注入开关,0为关闭,1为开启.
+sql_rule = ".*[; ]?((or)|(insert)|(select)|(union)|(update)|(delete)|(replace)|(create)|(drop)|(alter)|(grant)|(load)|(show)|(exec))[\\s(]" --sql防注入规则
+filte_file_type = "php|jsp";--禁止上传的文件后缀,注释则不过滤
